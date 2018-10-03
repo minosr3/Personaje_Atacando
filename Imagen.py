f20bbc5144c88fa60633b7e5344a8f85ee52eb64
minosr3 <39816912+minosr3@users.noreply.github.com>
2018-10-03 07:23:51 -0500

    Update Imagen.py

--- Imagen.py
+++ Imagen.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import *
 class Imagen(pygame.sprite.Sprite):
     def __init__(self,imagen1, imagen2, imagen3 ):
         self.imagen = imagen1
@@ -12,6 +13,9 @@ class Imagen(pygame.sprite.Sprite):
         self.rect.move_ip(self.posX, self.posY)
         self.cte = 0
     def accion(self):
+        accion = pygame.key.get_pressed()
+        if accion[K_z]:
+            self.cte=0
         if self.cte == 0:
             self.imagen = self.imagenes[1]
             self.cte = 1
@@ -19,5 +23,5 @@ class Imagen(pygame.sprite.Sprite):
             self.imagen = self.imagenes[2]
             self.cte=2
         elif self.cte == 2:
-            self.cte=0
+            self.cte=3
             self.imagen = self.imagenes[0]
